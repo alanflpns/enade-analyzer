220297b3cc0856b8bcd86999aef5cf1b135d38c4
alanflpns <alanflpns@gmail.com>
2022-01-07 01:12:20 -0300

    feat: add frontend

---
+++ frontend/src/services/Requests.tsx
@@ -0,0 +1,10 @@
+import AxiosApi from "./AxiosApi";
+
+const Requests = {
+  getData: () => {
+    const request = AxiosApi.get("result-enade-general?ano=2017&cod_ies=3183");
+    return request;
+  },
+};
+
+export default Requests;
