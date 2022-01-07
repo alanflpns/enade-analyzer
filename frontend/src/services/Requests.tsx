033b2a68258b166d03044e4720648e1fe0bac076
alanflpns <alanflpns@gmail.com>
2022-01-07 01:12:55 -0300

    fix: router request

--- frontend/src/services/Requests.tsx
+++ frontend/src/services/Requests.tsx
@@ -2,7 +2,7 @@ import AxiosApi from "./AxiosApi";
 
 const Requests = {
   getData: () => {
-    const request = AxiosApi.get("result-enade-general?ano=2017&cod_ies=3183");
+    const request = AxiosApi.get("/result-enade-general?ano=2017&cod_ies=3183");
     return request;
   },
 };
